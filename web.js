61d36151d0226a29b55b948ed5fb6fe116586b04
lsantana486 <lsantana486@vm-0.lsantana486.koding.kd.io>
2013-07-10 22:12:41 +0000

    Updating web.js to read index.html as a response

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var bfile = fs.readFileSync('index.html');
-  response.send(bfile.toString);
+  response.send(bfile.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
